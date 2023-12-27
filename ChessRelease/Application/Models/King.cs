e0a755599906cdf91b7dc4c7128fad4fb07c216b
khaustovdn <khaustovdn@vk.com>
2023-12-27 21:20:01 +0700

    Added some buttons in class King

--- ChessRelease/Application/Models/King.cs
+++ ChessRelease/Application/Models/King.cs
@@ -1,7 +1,12 @@
 namespace ChessRelease.Application.Models
 {
-    public class King
+    public class King : IFigure
     {
-        
+        public int number { get; set; } = 6;
+        public Button[,] buttons { get; set; } = new Button[8, 8];
+        public King(Button[,] butt)
+        {
+            buttons = butt;
+        }
     }
 }
