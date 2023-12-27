d1bac68ec2e13a9f1748b1794b10843518dc5e9a
Hikiray <satalomaksimka12345678910@gmail.com>
2023-12-28 00:06:59 +0700

    Добавил оснонвые свойства для класса Presenter

--- ChessRelease/Application/Presenter/IPresenter.cs
+++ ChessRelease/Application/Presenter/IPresenter.cs
@@ -1,7 +1,11 @@
+using System.Windows.Forms;
+
 namespace ChessRelease.Application.Presenter
 {
     public interface IPresenter
     {
-        
+        public Button prevButton;
+        public Button[,] buttons { get; set; } = new Button[8, 8];
+        public int[,] map;
     }
 }
