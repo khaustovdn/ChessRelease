631b4d183c9e2d579119652eb5a6e2ebc58bff90
Hikiray <satalomaksimka12345678910@gmail.com>
2023-12-28 00:11:32 +0700

    Добавил реализацию интерфейса

--- ChessRelease/Application/Presenter/IPresenter.cs
+++ ChessRelease/Application/Presenter/IPresenter.cs
@@ -4,8 +4,8 @@ namespace ChessRelease.Application.Presenter
 {
     public interface IPresenter
     {
-        public Button prevButton;
-        public Button[,] buttons { get; set; } = new Button[8, 8];
-        public int[,] map;
+        Button prevButton;
+        Button[,] buttons { get; set; } = new Button[8, 8];
+        int[,] map;
     }
 }
