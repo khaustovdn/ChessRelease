2c88675eee2b8f908cf83cfe19c5b7bf86b38c08
Sofiya <sofiatrifinova@gmail.com>
2023-12-27 23:05:17 +0700

    Добавил создание кнопок на доске

--- ChessRelease/Application/Models/Board.cs
+++ ChessRelease/Application/Models/Board.cs
@@ -14,6 +14,30 @@ namespace ChessRelease.Application.Models
         public Color prevColor { get; set; }
 
         public bool isMoving { get; set; } = false;
-        public Bitmap chessSprites { get; set; } = new Bitmap("C:\\Users\\satal\\OneDrive\\Рабочий стол\\chesspng\\chess.png");
+        public Bitmap chessSprites { get; set; } = new Bitmap("chess.png");
+        public Board()
+        {
+            map = new int[8, 8]
+            {
+                {15,14,13,12,11,13,14,15},
+                {16,16,16,16,16,16,16,16},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {0,0,0,0,0,0,0,0},
+                {26,26,26,26,26,26,26,26},
+                {25,24,23,22,21,23,24,25},
+            };
+        }
+        public Button CreateButton(int i, int j)
+        {
+            Button butt = new Button();
+            butt.Size = new Size(50, 50);
+            butt.Location = new Point(i * 50, j * 50);
+            butt.Click += new EventHandler(OnPressFigure);
+            butt = SkinButton(ColorChangedButton(butt, j, i), j, i);
+            buttons[j, i] = butt;
+            return butt;
+
     }
 }
