16a2ffb962ceac76ff3120779f2d771a6db4a395
Sofiya <sofiatrifinova@gmail.com>
2023-12-27 23:07:41 +0700

    Дописал класс Board

--- ChessRelease/Application/Models/Board.cs
+++ ChessRelease/Application/Models/Board.cs
@@ -112,5 +112,162 @@ namespace ChessRelease.Application.Models
                 }
             }
         }
+        private int[] FindKing()
+        {
+            int[] tmp = King.KingFor(map, player);
+            return tmp;
+        }
+        private void OnPressFigure(object sender, EventArgs e)
+        {
+            Button pressedButton = sender as Button; // чисто для удобства
+            prevColor = pressedButton.BackColor;
+
+            if (check == true)
+            {
+                pressedButton.BackColor = Color.Red;
+                DeactiveButtons();
+                ShowSteps(pressedButton.Location.Y / 50, pressedButton.Location.X / 50, map[pressedButton.Location.Y / 50, pressedButton.Location.X / 50]);
+                check = false;
+                isMoving = true;
+                pressedButton.Enabled = false;
+            }
+            else
+            {
+                if (map[pressedButton.Location.Y / 50, pressedButton.Location.X / 50] != 0 && player == map[pressedButton.Location.Y / 50, pressedButton.Location.X / 50] / 10)
+                {
+
+                    pressedButton.BackColor = Color.Red;
+                    DeactiveButtons();
+                    ShowSteps(pressedButton.Location.Y / 50, pressedButton.Location.X / 50, map[pressedButton.Location.Y / 50, pressedButton.Location.X / 50]);
+                    if (isMoving)
+                    {
+                        isMoving = false;
+                        ColorChangedAllButton();
+                        ActivateButtons();
+                    }
+                    else isMoving = true;
+
+                }
+                else
+                {
+                    if (isMoving == true)
+                    {
+
+                        // Меняем местами кнопки
+
+                        if (map[pressedButton.Location.Y / 50, pressedButton.Location.X / 50] != 0)
+                        {
+                            map[pressedButton.Location.Y / 50, pressedButton.Location.X / 50] = 0;
+                        }
+                        int temp = map[pressedButton.Location.Y / 50, pressedButton.Location.X / 50];
+                        map[pressedButton.Location.Y / 50, pressedButton.Location.X / 50] = map[prevButton.Location.Y / 50, prevButton.Location.X / 50];
+                        map[prevButton.Location.Y / 50, prevButton.Location.X / 50] = temp;
+                        pressedButton.BackgroundImage = prevButton.BackgroundImage;
+                        prevButton.BackgroundImage = null;
+                        isMoving = false;
+                        // Меняем местами кнопки\
+                        if (CheckSteps(pressedButton.Location.Y / 50, pressedButton.Location.X / 50, map))
+                        {
+                            ChangePlayer();
+                            ColorChangedAllButton();
+                            DeactiveButtons();
+                            check = true;
+                            int[] tmp = FindKing();
+                            buttons[tmp[0], tmp[1]].BackColor = Color.Red;
+                            buttons[tmp[0], tmp[1]].Enabled = true;
+                        }
+                        else
+                        {
+                            ChangePlayer();
+                            ColorChangedAllButton();
+                            ActivateButtons();
+                        }
+                    }
+                }
+            }
+            prevButton = pressedButton;
+        }
+        private void DeactiveButtons()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    buttons[i, j].Enabled = false;
+                }
+            }
+        }
+        private void ActivateButtons()
+        {
+            for (int i = 0; i < 8; i++)
+            {
+                for (int j = 0; j < 8; j++)
+                {
+                    buttons[i, j].Enabled = true;
+                }
+            }
+        }
+        private void ShowSteps(int IcurrFigure, int JcurrFigure, int currFigure)
+        {
+            switch (currFigure % 10)
+            {
+                case 6:
+                    Figure figure = new Figure(buttons);
+                    buttons = figure.Move(IcurrFigure, JcurrFigure, currFigure, map);
+                    buttons[IcurrFigure, JcurrFigure].Enabled = true;
+                    break;
+                case 5:
+                    Rook rook = new Rook(buttons);
+                    buttons = rook.Move(IcurrFigure, JcurrFigure, currFigure, map);
+                    buttons[IcurrFigure, JcurrFigure].Enabled = true;
+                    break;
+                case 4:
+                    Horse horse = new Horse(buttons);
+                    buttons = horse.Move(IcurrFigure, JcurrFigure, currFigure, map);
+                    buttons[IcurrFigure, JcurrFigure].Enabled = true;
+                    break;
+                case 3:
+                    Elephant elephant = new Elephant(buttons);
+                    buttons = elephant.Move(IcurrFigure, JcurrFigure, currFigure, map);
+                    buttons[IcurrFigure, JcurrFigure].Enabled = true;
+                    break;
+                case 2:
+                    Queen queen = new Queen(buttons);
+                    buttons = queen.Move(IcurrFigure, JcurrFigure, currFigure, map);
+                    buttons[IcurrFigure, JcurrFigure].Enabled = true;
+                    break;
+                case 1:
+                    King king = new King(buttons);
+                    buttons = king.Move(IcurrFigure, JcurrFigure, currFigure, map);
+                    buttons[IcurrFigure, JcurrFigure].Enabled = true;
+                    break;
+
+            }
+        }
+        private bool CheckSteps(int IcurrFigure, int JcurrFigure, int[,] map)
+        {
+            int currFigure = map[IcurrFigure, JcurrFigure] % 10;
+            switch (currFigure)
+            {
+                case 5:
+                    Rook rook = new Rook(buttons);
+                    return rook.Check(IcurrFigure,JcurrFigure,map);
+                case 6:
+                    Figure figure = new Figure(buttons);
+                    return figure.Check(IcurrFigure, JcurrFigure, map[IcurrFigure, JcurrFigure], map);
+                case 4:
+                    Horse horse = new Horse(buttons);
+                    return horse.Check(IcurrFigure, JcurrFigure,map[IcurrFigure, JcurrFigure],map);
+                case 3:
+                    Elephant elephant = new Elephant(buttons);
+                    return elephant.Check(IcurrFigure, JcurrFigure, map);
+                case 2:
+                    Queen queen = new Queen(buttons);
+                    return queen.Check(IcurrFigure, JcurrFigure, map);
+
+            }
+            return false;
+        }
+
     }
 }
