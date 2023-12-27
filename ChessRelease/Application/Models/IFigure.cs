5ea4b7591d4d6ae8ac5f75c1888f579b23b87e8a
Sofiya <sofiatrifinova@gmail.com>
2023-12-27 21:11:47 +0700

    Добавлен интерфейс и его реализация

--- ChessRelease/Application/Models/IFigure.cs
+++ ChessRelease/Application/Models/IFigure.cs
@@ -2,6 +2,8 @@ namespace ChessRelease.Application.Models
 {
     public interface IFigure
     {
-        
+        Button[,] buttons { get; set; }
+        int number {  get; set; }
+        Button[,] Move(int IcurrFigure, int JcurrFigure, int currFigure, int[,] map);
     }
 }
