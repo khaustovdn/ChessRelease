009927dcad3cecb8f1641d92e9955c252e6c4e6c
khaustovdn <khaustovdn@vk.com>
2023-12-27 21:18:52 +0700

    Added buttons

--- ChessRelease/Application/Models/Horse.cs
+++ ChessRelease/Application/Models/Horse.cs
@@ -1,7 +1,12 @@
 namespace ChessRelease.Application.Models
 {
-    public class Horse
+    public class Horse : IFigure
     {
-        
+        public int number { get; set; } = 6;
+        public Button[,] buttons { get; set; } = new Button[8, 8];
+        public Horse(Button[,] butt)
+        {
+            buttons = butt;
+        }
     }
 }
