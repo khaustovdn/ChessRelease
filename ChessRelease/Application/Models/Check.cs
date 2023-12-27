5e65e3d4ed7d30e0b5f9aef41b17aa62783e0f7e
Hikiray <satalomaksimka12345678910@gmail.com>
2023-12-27 23:50:45 +0700

    Добавлен класс проверки
    Signed-off-by: Sofiya <sofiatrifinova@gmail.com>

--- ChessRelease/Application/Models/Check.cs
+++ ChessRelease/Application/Models/Check.cs
@@ -2,6 +2,11 @@ namespace ChessRelease.Application.Models
 {
     public class Check
     {
-        
+        public static bool InsideBorder(int ti, int tj)
+        {
+            if (ti >= 8 || tj >= 8 || ti < 0 || tj < 0)
+                return false;
+            return true;
+        }
     }
 }
