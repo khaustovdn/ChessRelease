02cbc183080be3b1788c3a055fdc6d30efa13599
Hikiray <satalomaksimka12345678910@gmail.com>
2023-12-28 00:50:25 +0700

    Готово

--- ChessRelease/Form1.cs
+++ ChessRelease/Form1.cs
@@ -1,20 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
+﻿using System.Windows.Forms;
+using ChessRelease.Application.Models;
 
-namespace ChessRelease
+namespace ChessRelease;
+
+public partial class Form1 : Form
 {
-    public partial class Form1 : Form
+    public Board board { get; set; }
+    public Form1()
     {
-        public Form1()
+        board = new Board();
+        InitializeComponent();
+        CreateMap();
+    }
+    public void CreateMap()
+    {
+        for (int i = 0; i < 8; i++)
         {
-            InitializeComponent();
+            for (int j = 0; j < 8; j++)
+            {
+                Controls.Add(board.CreateButton(i, j));
+            }
         }
     }
 }
