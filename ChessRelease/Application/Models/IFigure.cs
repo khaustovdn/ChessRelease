938a895a429b1e27ef8da5bae34adfd1d9d1a699
Sofiya <sofiatrifinova@gmail.com>
2023-12-27 21:34:52 +0700

    Подключена библиотка Forms для кнопок

--- ChessRelease/Application/Models/IFigure.cs
+++ ChessRelease/Application/Models/IFigure.cs
@@ -1,3 +1,5 @@
+using System.Windows.Forms;
+
 namespace ChessRelease.Application.Models
 {
     public interface IFigure
--- ChessRelease/Application/Models/Steps.cs
+++ ChessRelease/Application/Models/Steps.cs
@@ -1,7 +1,15 @@
+using System.Drawing;
+using System.Windows.Forms;
+
 namespace ChessRelease.Application.Models
 {
     public class Steps
     {
-        
+        public static Button StepColor(Button butt)
+        {
+            butt.BackColor = Color.Yellow;
+            butt.Enabled = true;
+            return butt;
+        }
     }
 }
