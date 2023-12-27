f495d55cb1a7a23d95340cecddeaba5006ee0ccb
khaustovdn <khaustovdn@vk.com>
2023-12-27 21:42:03 +0700

    Added constructor and vars for class Queen

--- ChessRelease/Application/Models/Queen.cs
+++ ChessRelease/Application/Models/Queen.cs
@@ -1,7 +1,14 @@
+using System.Windows.Forms;
+
 namespace ChessRelease.Application.Models
 {
     public class Queen
     {
-        
+        public int number { get; set; } = 6;
+        public Button[,] buttons { get; set; } = new Button[8, 8];
+        public Queen(Button[,] butt)
+        {
+            buttons = butt;
+        }
     }
 }
